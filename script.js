3b4fee2802df2a7c56582425af4171776dc618fb
cshurney <cshurney45@gmail.com>
2016-10-27 15:27:56 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+alert('hello world');
+
+var F = 32
+var C = (F-32)*(5/9)
+
+console.log(C+" degrees outside it is freezing!");
+
+var C = 75
+var F = (C+32)*(9/5)
+
+console.log(F+" degrees outside it is HOT!");
